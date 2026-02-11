ae958708d2c90544ee582135257a724839b0f541
Zaaca-prog <zoggisaac6@gmail.com>
2026-02-11 11:49:49 +0700

    inches and meters added to ultrasonic file servo now has deintialization and aattaching pins

--- iothlpr_Servo.py
+++ iothlpr_Servo.py
@@ -1,9 +1,26 @@
-from machine import Pin,PWM
+from typing import Self
+from machine import Pin,PWM 
 class Servo():
-    def __init__(self, pin, Freq, max_angle=180) -> None:
-        self.pwm = PWM(pin,freq=Freq)
+    def __init__(self, pin:Pin = None, Freq:int = 50, max_angle:int=180) -> None:
+        self.pwm = None
+        if pin != None:
+            self.pwm = PWM(pin)
+            self.pwm.freq(Freq)
+            self.pwm.duty(0)
         self.max_angle = max_angle
+        self.angle = self.pwm.duty()/1023 * 180
     def write_angle(self, angle):
-        self.pwm.duty(angle/self.max_angle*1023)
+        try:
+            self.pwm.duty(int(angle/self.max_angle*1023))
+        except OSError:
+            print("cannot write angle (may be deinitialized currently)")
 
-    
+    def attach(self,pin:Pin) -> None:
+        self.pwm = PWM(pin)
+        self.pwm.freq(50)
+        self.pwm.duty(0)
+    def detach(self) -> None:
+        if not isinstance(self.pwm,PWM):
+            print(f"Error: Invalid data type. Expected PWM, got {type(self.pwm).__name__}.", file=sys.stderr)
+        else:
+            self.pwm.deinit()
--- iothlpr_UltraSonic.py
+++ iothlpr_UltraSonic.py
@@ -1,17 +1,35 @@
-from machine import Pin,time_pulse_us
-import utime
+from machine import Pin,time_pulse_us import utime class UltraSonic(): def __init__(self, trigpin ,echopin) -> None: self.echo_pin = echopin self.trig_pin = trigpin self.trigger = Pin(trigpin,Pin.OUT)
 class UltraSonic():
-    def __init__(self, trigpin ,echopin) -> None:
-        self.echo_pin = echopin
-        self.trig_pin = trigpin
-        self.trigger = Pin(trigpin,Pin.OUT)
+    def __init__(self,trig:Pin,echo:Pin):
+        self.trig_pin = trig
+        self.echo_pin = echo 
     def get_cm(self) -> float:
-        self.trigger.value(0) 
+        self.trig_pin.value(0) 
         utime.sleep_us(2)
-        self.trigger.value(1)
+        self.trig_pin.value(1)
         utime.sleep_us(10)
-        self.trigger.value(0)
+        self.trig_pin.value(0)
             
         duration = time_pulse_us(self.echo_pin,1,3000)
         cm = duration * 0.0343 / 2 # gets cm
-        return cm 
+        return cm
+    def get_inches(self) -> float:
+        self.trig_pin.value(0) 
+        utime.sleep_us(2)
+        self.trig_pin.value(1)
+        utime.sleep_us(10)
+        self.trig_pin.value(0)
+            
+        duration = time_pulse_us(self.echo_pin,1,3000)
+        inches = duration * 0.0343 *2.54 / 2 # gets cm
+        return inches
+    def get_meters(self) -> float:
+        self.trig_pin.value(0) 
+        utime.sleep_us(2)
+        self.trig_pin.value(1)
+        utime.sleep_us(10)
+        self.trig_pin.value(0)
+            
+        duration = time_pulse_us(self.echo_pin,1,3000)
+        meters = duration * 0.0343 / 200  # gets meters 200 = x/100/2
+        return  meters
